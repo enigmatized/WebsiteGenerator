be9d27af7cdd2a0d18070bc56109abe75b8223e4
enigmatized <going.to.any.lengths.work@gmail.com>
2025-07-04 17:30:47 -0500

    Changing config loader to choose config loading based on build commands

--- src/config.js
+++ src/config.js
@@ -1,13 +1,30 @@
 // config-loader.js
 // import config1 from './communities/community1.js';
-import config2 from './configDropEverything.json';
+import configDropEverything2 from './configDropEverything.json';
 import configDropEverything from './configDropEverything.json';
 
 //TODO figure out a way to import with multiple cloudflares. Looks like command line arg should be used to to decide the config file
 
 //const hostname = window.location.hostname;
 
+
+const community = import.meta.env.VITE_COMMUNITY;
+
 //const config = hostname.includes('local') ? config2 : config1;
-const config =  config2;
+//const config =  config2;
+
+let config;
+
+switch (community) {
+  case 'group1':
+    config = configDropEverything;
+    break;
+  case 'group2':
+    config = configDropEverything;
+    break;
+  default:
+    config = { home: '<h1>Error</h1>', about: '<h2>Error</h2>' };
+}
+
 export default config;
 
