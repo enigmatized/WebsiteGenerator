353b2fd449b993461df9cc857b55481da7e761a9
enigmatized <going.to.any.lengths.work@gmail.com>
2025-07-05 15:43:12 -0500

    Adding Second config file for the army of other websites

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import config from './config';
 
 
 //TODO Add how it works to Commonly asked questions
-const tabs = ['Home', 'About', 'Contact','Commonly Asked Questions','Meeting Schedule'];
+const tabs = ['Home', 'About', 'Commonly Asked Questions','Meeting Schedule', 'Contact'];
 if (config.support) {
   tabs.push('Support Project');
 }
@@ -18,18 +18,23 @@ if (config.bookFile) {
 }
 
 
+const Calendar = ({ calendarUrl }) => {
+  return calendarUrl ? (
+    <div> <iframe
+      title="Meeting Schedule"
+      src={calendarUrl}
+      style={{ border: 0, width: '100%', height: 600 }}
+      frameBorder="0"
+      scrolling="no"
+      loading="lazy"
+    /> <p><a href="https://us06web.zoom.us/j/5620489534"> Zoom Link For All the Meetings: https://us06web.zoom.us/j/5620489534  </a> </p> </div>
+  ) : (
+    <p>No calendar available for this community.</p>
+  );
+};
 
 
-const Calendar = () => (
-  <iframe
-    title="Meeting Schedule"
-    src="https://calendar.google.com/calendar/embed?src=drop.all.addictions.fellowship%40gmail.com&ctz=America%2FLos_Angeles"
-    style={{ border: 0, width: '100%', height: 600 }}
-    frameBorder="0"
-    scrolling="no"
-    loading="lazy"
-  />
-);
+
 
 
 
@@ -41,12 +46,11 @@ const getTabContent = (tab) => {
       //case 'Home': return config.home;
       case 'Home': return <div dangerouslySetInnerHTML={{ __html: config.home  || 'Not found.' }} />;
 
-      case 'About': return config.about;
+      case 'About': return <div dangerouslySetInnerHTML={{ __html: config.about  || 'Not found.' }} />;
       case 'How It Works' : return  config.howitworks;
       case 'Contact': return <div dangerouslySetInnerHTML={{ __html: config.contact  || 'Not found.' }} />;
       case 'Commonly Asked Questions': return <div dangerouslySetInnerHTML={{ __html:  config.commonQuestions || 'Not found.' }} />; 
-      case 'Meeting Schedule':return <Calendar />; 
-		    //case 'Meeting Schedule': return config.meetingSchedule;
+      case 'Meeting Schedule': return <Calendar calendarUrl={ config.googleIframe } />; 
       case 'Book': return <BookReader file={`/${config.bookFile}`} />;
       case 'Support Project': return <div dangerouslySetInnerHTML={{ __html: config.support  || 'Not found.' }} />;
 
--- src/config.js
+++ src/config.js
@@ -1,17 +1,8 @@
-// config-loader.js
-// import config1 from './communities/community1.js';
-import configDropEverything2 from './configDropEverything.json';
 import configDropEverything from './configDropEverything.json';
-
-//TODO figure out a way to import with multiple cloudflares. Looks like command line arg should be used to to decide the config file
-
-//const hostname = window.location.hostname;
-
+import configLivingMeditation from './configLivingMeditation.json';
 
 const community = import.meta.env.VITE_COMMUNITY;
 
-//const config = hostname.includes('local') ? config2 : config1;
-//const config =  config2;
 
 let config;
 
@@ -20,7 +11,7 @@ switch (community) {
     config = configDropEverything;
     break;
   case 'group2':
-    config = configDropEverything;
+    config = configLivingMeditation;
     break;
   default:
     config = { home: '<h1>Error</h1>', about: '<h2>Error</h2>' };
